eba8926956f11a1f2c10389cccc735ed931ef0d3
igorklopov <igor@klopov.com>
2015-04-27 23:40:45 +0400

    0.2.21: better sequelize

---
+++ dictionary/pg.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  scripts: [
+    "lib/**/*.js"
+  ],
+
+  patches: {
+
+    "lib/native/index.js": [
+      "require('pg-native')",
+      "require('pg-native', 'can-ignore')"
+    ]
+
+  }
+
+};
--- dictionary/sequelize.js
+++ dictionary/sequelize.js
@@ -6,6 +6,19 @@ module.exports = {
 
   patches: {
 
+    "lib/dialects/sqlite/connection-manager.js": [
+      "this.lib = require(sequelize.config.dialectModulePath || 'sqlite3').verbose();",
+      "var dmp = sequelize.config.dialectModulePath;" +
+      "this.lib = (dmp ? require(dmp) : require('sqlite3')).verbose();"
+    ],
+
+    "lib/dialects/postgres/connection-manager.js": [
+      "this.lib = sequelize.config.native ? require(sequelize.config.dialectModulePath || 'pg').native : require(sequelize.config.dialectModulePath || 'pg');",
+      "var dmp = sequelize.config.dialectModulePath;" +
+      "var lib = dmp ? require(dmp) : require('pg');" +
+      "this.lib = sequelize.config.native ? lib.native : lib;"
+    ],
+
     "lib/dialects/postgres/hstore.js": [
       "require('pg-hstore')",
       "require('pg-hstore', 'can-ignore')"
